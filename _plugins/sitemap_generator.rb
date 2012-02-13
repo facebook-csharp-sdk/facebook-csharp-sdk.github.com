08d4825e0ddf9fd7cad1e1159fafbe8738e5b4b2
Nathan Totten <ntotten@microsoft.com>
2012-02-13 13:51:03 -0800

    Added sitemap plugin

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -39,7 +39,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://www.mysite.com"
+  MY_URL = "http://docs.csharpsdk.org"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
